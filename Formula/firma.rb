0990531b1cef6e3d7be241e323e50acfe643dae5
federicobassetto <federico_bassetto@external.mckinsey.com>
2026-05-26 09:05:46 +0200

    fix brew

--- Formula/firma.rb
+++ Formula/firma.rb
@@ -5,23 +5,23 @@ class Firma < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/Firma-AI/openfirma/releases/download/v0.0.0/firma-0.0.0-aarch64-apple-darwin.tar.gz"
-      sha256 "0121c443c87a50ca37f585669a8873447c44772028fcd2fbc7e36891badb0793"
+      url "https://github.com/Firma-AI/openfirma/releases/download/v0.1.0/firma-0.1.0-aarch64-apple-darwin.tar.gz"
+      sha256 "REPLACE_AFTER_RELEASE"
     end
     on_intel do
-      url "https://github.com/Firma-AI/openfirma/releases/download/v0.0.0/firma-0.0.0-x86_64-apple-darwin.tar.gz"
-      sha256 "fa681b0268b9be21e514b28a6afccd61c694ec91ed34af3b29ffb9cf03a3967b"
+      url "https://github.com/Firma-AI/openfirma/releases/download/v0.1.0/firma-0.1.0-x86_64-apple-darwin.tar.gz"
+      sha256 "REPLACE_AFTER_RELEASE"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/Firma-AI/openfirma/releases/download/v0.0.0/firma-0.0.0-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "184158ee50fd2beaa12478f7e67b2f811c173cce5ef431b87a8aaa6a15373a56"
+      url "https://github.com/Firma-AI/openfirma/releases/download/v0.1.0/firma-0.1.0-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "REPLACE_AFTER_RELEASE"
     end
     on_intel do
-      url "https://github.com/Firma-AI/openfirma/releases/download/v0.0.0/firma-0.0.0-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "bf9d24956e70c49842a421684ebe331ede6282dacb7526b074ee2642976d2d3a"
+      url "https://github.com/Firma-AI/openfirma/releases/download/v0.1.0/firma-0.1.0-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "REPLACE_AFTER_RELEASE"
     end
   end
 
@@ -31,5 +31,6 @@ class Firma < Formula
 
   test do
     assert_match version.to_s, shell_output("#{bin}/firma --version")
+    assert_match "config", shell_output("#{bin}/firma --help")
   end
 end
